27ad07a73a1bbf87ad35caf798aee4c6ce74fa14
Ray <91828843+Alphocado@users.noreply.github.com>
2025-06-08 12:17:50 +0800

    perbaiki styling

--- includes/layout_anggota.php
+++ includes/layout_anggota.php
@@ -19,7 +19,7 @@ function renderHeader($title = "Sistem Perpustakaan", $activeMenu = "") {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title; ?> - Perpustakaan</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../assets/css/includes/layout_anggota.css">
+  <link rel="stylesheet" href="../assets/css/includes/layout.css">
   
 </head>
 <body>
